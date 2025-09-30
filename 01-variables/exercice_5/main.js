d792fff5ef0ab93485c09929d60b6684e957fe07
ElSheria <sheriaelie@gmail>
2025-09-30 16:04:48 +0200

    fix: define and export the celsiusToFahrenheit function to pass the test

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,6 +1,14 @@
 // Exercice 5
 // Écrivez votre code ici
 
+// Définir la fonction
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+// Tester avec une valeur fixe
 let celsius = 25;
-let fahrenheit = (celsius * 9 / 5) + 32;
-console.log(celsius + "°C = " + fahrenheit + "°F");
+console.log(celsius + "°C équivaut à " + celsiusToFahrenheit(celsius) + "°F");
+
+// Exporter la fonction pour les tests
+module.exports = celsiusToFahrenheit;
